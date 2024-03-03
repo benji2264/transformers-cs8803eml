dc32ba0882f551cfb4f209a8d18617b9667a4a1e
benji2264 <benji2264@gmail.com>
2024-03-03 18:50:35 -0500

    debug quantize

--- examples/pytorch/text-classification/run_glue.py
+++ examples/pytorch/text-classification/run_glue.py
@@ -665,6 +665,8 @@ def main():
         
         # Replace original linear layer with quantized version
         setattr(model, name, quantized_linear)
+
+    for name, module in model.named_modules():
         print(name, type(module))
 
     # Evaluation
